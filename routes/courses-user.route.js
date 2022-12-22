0996ecf958aaad652f9ccb140ef1d56e0b7c2ebf
SonTran2002 <tranminhson456@gmail.com>
2022-12-22 10:39:11 +0700

    add search function and fix

--- routes/courses-user.route.js
+++ routes/courses-user.route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import coursesService from "../services/courses.service.js";
+import * as bodyParser from "express";
 
 const router = express.Router();
 
@@ -40,6 +41,49 @@ router.get("/byCat/:id", async function (req, res) {
     pageNumbers: pageNumbers,
   });
 });
+
+router.get('/search',async function (req,res){
+  res.render('vwCourses/search');
+});
+
+router.post('/search', async function (req, res) {
+  const ret=req.body.Search;
+  console.log(ret);
+  console.log(req.body);
+  const product = await coursesService.searchByName(ret);
+const CourCount= await coursesService.countsearch(ret);
+  if (product === null) {
+    return res.redirect('/');
+  }
+  const catId = res.locals.CourCount.CatName;
+console.log(catId);
+  for (let c of res.locals.lcCategories) {
+    if (c.CatID === +catId) c.isActive = true;
+  }
+
+  const curPage = req.query.page || 1;
+  const limit = 6;
+  const offset = (curPage - 1) * limit;
+
+  const total = await coursesService.countByCatId(catId);
+  const nPages = Math.ceil(total / limit);
+
+  const pageNumbers = [];
+  for (let i = 1; i <= nPages; i++) {
+    pageNumbers.push({
+      value: i,
+      isCurrent: i === +curPage,
+    });
+  }
+
+  const list = await coursesService.findPageByCatId(catId, limit, offset);
+  res.render('vwCourses/search', {
+    product: product,
+    CourCount:CourCount
+  });
+
+});
+
 router.get('/detail/:id', async function (req, res) {
   const proId = req.params.id || 0;
   const product = await coursesService.findById(proId);
